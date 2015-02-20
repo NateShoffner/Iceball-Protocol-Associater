1c6b20f77646f2237b944d5c6a267c49fe1b364f
Nate Shoffner <nate.shoffner@gmail.com>
2015-02-19 21:02:42 -0500

    Initial commit.

---
+++ Iceball Protocol Associater/MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Iceball_Protocol_Associater
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(333, 24);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Iceball directory:";
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(291, 93);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(117, 23);
+            this.btnApply.TabIndex = 3;
+            this.btnApply.Text = "Apply Association";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(12, 93);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(117, 23);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remove Association";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 52);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(166, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Use ENet (uncheck if unsure)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 128);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Iceball Protocol Associater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Iceball Protocol Associater/MainForm.cs
@@ -0,0 +1,82 @@
+﻿#region
+
+using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+#endregion
+
+namespace Iceball_Protocol_Associater
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+            LoadAssociation();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog() {Description = "Locate Iceball Directory"})
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void LoadAssociation()
+        {
+            var key = Registry.ClassesRoot.OpenSubKey("iceball\\shell\\open\\command");
+
+            if (key != null)
+            {
+                var command = key.GetValue(null).ToString();
+                var enetEnabled = command.Contains(" -c ");
+
+                var cmdPath = command.Substring(1, command.IndexOf(".exe") + 3);
+
+                checkBox1.Checked = enetEnabled;
+                textBox1.Text = Path.GetDirectoryName(cmdPath);
+            }
+        }
+
+        private void SaveAssocation()
+        {
+            var enetFlag = checkBox1.Checked ? "-c" : "-C";
+            var cmdValue = string.Format("\"{0}\" {1} %1", Path.Combine(textBox1.Text, "iceball.exe"), enetFlag);
+
+            var classKey = Registry.ClassesRoot.CreateSubKey("iceball\\");
+            classKey.SetValue(null, "URL:Iceball Protocol Handler");
+            classKey.SetValue("URL Protocol", "");
+
+            var commandKey = Registry.ClassesRoot.CreateSubKey("iceball\\shell\\open\\command");
+            commandKey.SetValue(null, cmdValue);
+        }
+
+
+        private void RemoveAssociation()
+        {
+            Registry.ClassesRoot.DeleteSubKeyTree("iceball\\");
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            RemoveAssociation();
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox1.Text))
+            {
+                SaveAssocation();
+            }
+        }
+    }
+}
